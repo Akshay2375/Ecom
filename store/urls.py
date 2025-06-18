14e529d5ee81b179cc0e7f233a8fe3b0c03ffeaf
Akshay2375 <mundaleakshay7@gmail.com>
2025-06-18 12:51:39 +0530

    Registration

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 
 urlpatterns = [
-   path('home/',views.home,name="home"),
+  
+   path('',views.home,name="home"),
    path('about/',views.about,name="about"),
+   path('login/',views.login_user,name="login"),
+   path('logout/',views.logout_user,name="logout"),
+   path('register/',views.register_user,name="register"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
 from .models import Product
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import forms
+
+from .forms import SignUpForm
+
+
+# @login_required(login_url='login/')
 def home(request):
     products=Product.objects.all()
     return render(request,'home.html',{'products':products})
@@ -8,4 +16,48 @@ def home(request):
 def about(request):
     return render(request,'about.html',{})
 
+def login_user(request):
+
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,(" YOU ARE LOGGED IN "))
+            return redirect('home')
+        else:
+             messages.success(request,(" INVALID CREDENTIALS  "))
+             return redirect('login')
+    else:
+        return render(request,"login.html")
+
+     
+def logout_user(request):
+
+    logout(request)
+    messages.success(request,(" LOGEED OUT  "))
+    return redirect('login')
+
+
+def register_user(request):
+    form=SignUpForm()
+
+    if request.method=="POST":
+        form=SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,(" U HAVe become a user  "))
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+            user=authenticate(username=username,password=password)
+            login(request,user)
+            return redirect('home')
+        else:
+         messages.success(request,(" Problem REGISTER AGAIN  "))
+         return redirect('register')
+    else:
+
+
+     return render(request,"registration.html",{'form':form})
  
